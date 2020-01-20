df47fa26560e05fdf3c4b9392206b5a06a0b45a6
kamel bouali <bouali.kamel@grp-sonelgaz.sng>
2020-01-20 09:01:30 +0100

    adding basemap and webtile layer test

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,7 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
 module.exports = function override(config, env) {
     config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin))
-    config.plugins.push(new ArcGISPlugin())
+    config.plugins.push(new ArcGISPlugin({useDefaultAssetLoaders: false}))
     config.node.global=false
     return config;
   }
--- src/data/MapConfig.js
+++ src/data/MapConfig.js
@@ -1,17 +1,32 @@
-import WebMap from "esri/WebMap";
 import MapView from "esri/views/MapView";
 import Search from "esri/widgets/Search";
+import Basemap from "esri/Basemap"
+import WebTileLayer from "esri/layers/WebTileLayer"
+import Map from "esri/Map"
 
 const noop = () => {};
 
-export const webmap = new WebMap({
-  portalItem: {
-    id: "974c6641665a42bf8a57da08e607bb6f"
-  }
+const defaultBasemap = new Basemap({
+  baseLayers: [new WebTileLayer({
+      urlTemplate: "http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{level}/{row}/{col}.png"
+  })],
+  title: "blablabalb",
+  id: "streets",
+  thumbnailUrl: "https://stamen-tiles.a.ssl.fastly.net/terrain/10/177/410.png"
 });
 
+
 export const view = new MapView({
-  map: webmap
+  container: "mapDiv", // Reference to the map div 
+  map: new Map({
+      basemap: defaultBasemap
+  }), // Reference to the map object created before the scene
+  zoom: 6, // Sets zoom level based on level of detail (LOD)            
+  center: [3.262939, 36.618283], // Sets center point of view using longitude,latitude
+  ui: {
+      components: ["zoom", "compass"]
+  }
+
 });
 
 export const search = new Search({ view });
@@ -22,7 +37,7 @@ export const initialize = (container) => {
   view
     .when()
     .then(_ => {
-      console.log("Map and View are ready");
+      console.log("Map and View and Search are ready");
     })
     .catch(noop);
   return () => {
